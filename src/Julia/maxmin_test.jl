fb57c017eed689dc5bd80ed7b7746052f17e8eec
ShimShtern <shimrits@technion.ac.il>
2021-08-23 16:13:04 +0300

    update maxmin_test

--- src/Julia/maxmin_test.jl
+++ src/Julia/maxmin_test.jl
@@ -15,7 +15,8 @@ bLOAD_FROM_FILE=false
 t = [62; 54; 100]
 #β = 0.01
 β = 0
-μ = 1.15
+μ = 1.10
+gamma_const = 0.04
 δ = 0
 file = matopen("Patient4_Visit1_16beams_withdeadvoxels.mat") #changed from 13 since it did not cover the PTV
 γ = read(file,"neighbors_Mat")
@@ -75,9 +76,9 @@ phi_bar = ϕ.+ δ
 phi_bar[phi_bar.>1].= 1
 
 println("Now solving with min phi bar = ", minimum(phi_bar), " min phi_under = " , minimum(phi_under), " max phi_bar = ", maximum(phi_bar), " max phi_under = ", maximum(phi_under))
-@time model=maxmin_twostage_subprob.robustCuttingPlaneAlg(D,firstIndices,t,dvrhs,β,μ,γ,phi_under,phi_bar,200,bLOAD_FROM_FILE)
+@time model=maxmin_twostage_subprob.robustCuttingPlaneAlg(D,firstIndices,t,dvrhs,β,μ,γ,gamma_const,phi_under,phi_bar,200,bLOAD_FROM_FILE)
 xx = value.(model[:x])
 g = value.(model[:g])
 PhysHom=maximum(D[1:firstIndices[1]-1,:]*xx)/minimum(D[1:firstIndices[1]-1,:]*xx)
-file_name=@sprintf("results_%1.2f_%.1f_%.2f_%.2f.jld2",β,μ,δ,gamma_const)
-FileIO.save(("file_name","xx",xx,"g",g,"PhysHom",PhysHom,"phi_under",phi_under,"phi_bar",phi_bar,"t",t,"δ",δ,"μ",μ,"β",β,"gamma_const",gamma_const)
+file_name=@sprintf("results_%1.2f_%.2f_%.2f_%.2f.jld2",β,μ,δ,gamma_const)
+FileIO.save(file_name,"xx",xx,"g",g,"PhysHom",PhysHom,"phi_under",phi_under,"phi_bar",phi_bar,"t",t,"δ",δ,"μ",μ,"β",β,"gamma_const",gamma_const)
